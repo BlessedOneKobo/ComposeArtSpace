c2d16c355ff52fcfc957b06719d06362d76fa43a
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2023-07-28 00:14:45 +0100

    feat: create static ArtworkWall and ArtSpaceApp

--- app/src/main/java/com/example/composeartspace/MainActivity.kt
+++ app/src/main/java/com/example/composeartspace/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -19,11 +21,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeartspace.ui.theme.ComposeArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DisplayController()
+                    ComposeArtSpaceApp()
                 }
             }
         }
@@ -45,9 +52,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DisplayControllerButton(
-    @StringRes textId: Int,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
+    @StringRes textId: Int, onClick: () -> Unit, modifier: Modifier = Modifier
 ) {
     Button(onClick = onClick, modifier = modifier.width(128.dp)) {
         Text(text = stringResource(id = textId))
@@ -60,21 +65,16 @@ fun DisplayController(modifier: Modifier = Modifier) {
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = modifier.fillMaxWidth()
     ) {
-        DisplayControllerButton(
-            textId = R.string.previous_button_text,
+        DisplayControllerButton(textId = R.string.previous_button_text,
             onClick = {})
-        DisplayControllerButton(
-            textId = R.string.next_button_text,
+        DisplayControllerButton(textId = R.string.next_button_text,
             onClick = {})
     }
 }
 
 @Composable
 fun ArtDescriptor(
-    name: String,
-    artist: String,
-    year: Int,
-    modifier: Modifier = Modifier
+    name: String, artist: String, year: Int, modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier
@@ -104,6 +104,69 @@ fun ArtDescriptor(
     }
 }
 
+@Composable
+fun ArtworkWall(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(360.dp),
+        shadowElevation = 12.dp,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.wallhaven_72rxqo),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier.padding(all = 32.dp),
+        )
+    }
+}
+
+@Composable
+fun ComposeArtSpaceApp() {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+    ) {
+        ArtworkWall(
+            modifier = Modifier
+                .padding(bottom = 52.dp)
+        )
+        Column {
+            ArtDescriptor(
+                name = "Still Life of Blue Rose and Other Flowers",
+                artist = "Owen Scott",
+                year = 2021,
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            DisplayController(
+                modifier = Modifier.padding(
+                    start = 12.dp,
+                    end = 12.dp
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArtSpaceAppPreview() {
+    ComposeArtSpaceTheme {
+        ComposeArtSpaceApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArtworkWallPreview() {
+    ComposeArtSpaceTheme {
+        ArtworkWall()
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ArtDescriptorPreview() {
@@ -128,8 +191,7 @@ fun DisplayControllerPreview() {
 @Composable
 fun DisplayControllerButtonPreview() {
     ComposeArtSpaceTheme {
-        DisplayControllerButton(
-            textId = R.string.previous_button_text,
+        DisplayControllerButton(textId = R.string.previous_button_text,
             onClick = {})
     }
 }
