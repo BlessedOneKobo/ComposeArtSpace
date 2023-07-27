604d4cc0a5a8dc40194916aeb4de7447f7015b32
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2023-07-27 22:20:39 +0100

    feat: create static DisplayController

--- app/src/main/java/com/example/composeartspace/MainActivity.kt
+++ app/src/main/java/com/example/composeartspace/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.composeartspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeartspace.ui.theme.ComposeArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,5 +33,49 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun DisplayControllerButton(
+    @StringRes textId: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(onClick = onClick, modifier = modifier.width(128.dp)) {
+        Text(text = stringResource(id = textId))
+    }
+}
+
+@Composable
+fun DisplayController(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        DisplayControllerButton(
+            textId = R.string.previous_button_text,
+            onClick = {})
+        DisplayControllerButton(
+            textId = R.string.next_button_text,
+            onClick = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayControllerPreview() {
+    ComposeArtSpaceTheme {
+        DisplayController()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayControllerButtonPreview() {
+    ComposeArtSpaceTheme {
+        DisplayControllerButton(
+            textId = R.string.previous_button_text,
+            onClick = {})
     }
 }
