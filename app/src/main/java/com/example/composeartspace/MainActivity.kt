f2ca6cc26eebd4058f42390eaff71af0055305a1
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2023-07-27 23:25:35 +0100

    feat: create static ArtDescriptor

--- app/src/main/java/com/example/composeartspace/MainActivity.kt
+++ app/src/main/java/com/example/composeartspace/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeartspace.ui.theme.ComposeArtSpaceTheme
@@ -30,6 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    DisplayController()
                 }
             }
         }
@@ -59,6 +66,53 @@ fun DisplayController(modifier: Modifier = Modifier) {
         DisplayControllerButton(
             textId = R.string.next_button_text,
             onClick = {})
+    }
+}
+
+@Composable
+fun ArtDescriptor(
+    name: String,
+    artist: String,
+    year: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.secondaryContainer)
+            .padding(
+                start = 16.dp,
+                top = 20.dp,
+                bottom = 20.dp,
+                end = 64.dp,
+            )
+    ) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Light,
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Row {
+            Text(text = artist, style = MaterialTheme.typography.labelLarge)
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "($year)",
+                style = MaterialTheme.typography.labelLarge,
+                fontWeight = FontWeight.Light
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArtDescriptorPreview() {
+    ComposeArtSpaceTheme {
+        ArtDescriptor(
+            name = "Still Life of Blue Rose and Other Flowers",
+            artist = "Owen Scott",
+            year = 2021,
+        )
     }
 }
 
